451f1d00926d88a54f8c7049bffc02ef1363b8b3
JMcCarthy <uvatmvf@gmail.com>
2024-06-09 08:26:58 -0600

    add implementation tutorial code

--- Labfiles/04-face/C-Sharp/README.md
+++ Labfiles/04-face/C-Sharp/README.md
@@ -1,4 +1,5 @@
 This folder contains C# code
 
 # To run
-1.  dotnet add package Azure.AI.Vision.ImageAnalysis -v 0.15.1-beta.1
+1. dotnet add package Azure.AI.Vision.ImageAnalysis -v 0.15.1-beta.1
+2. dotnet run
--- Labfiles/04-face/C-Sharp/computer-vision/Program.cs
+++ Labfiles/04-face/C-Sharp/computer-vision/Program.cs
@@ -29,7 +29,9 @@ namespace detect_people
                 }
 
                 // Authenticate Azure AI Vision client
-
+                var cvClient = new VisionServiceOptions(
+                    aiSvcEndpoint,
+                    new AzureKeyCredential(aiSvcKey));
                 
                 // Analyze image
                 AnalyzeImage(imageFile, cvClient);
@@ -47,11 +49,67 @@ namespace detect_people
 
             var analysisOptions = new ImageAnalysisOptions()
             {
-                // Specify features to be retrieved
+                // Specify features to be retrieved (PEOPLE)
+                Features =
+                    ImageAnalysisFeature.People
 
             };
+            VisionSource imageSource;
+            ImageAnalyzer analyzer;
+            get_image_analysis(imageFile, serviceOptions, analysisOptions, out imageSource, out analyzer);
 
-            // Get image analysis
+            static void get_image_analysis(string imageFile, VisionServiceOptions serviceOptions, ImageAnalysisOptions analysisOptions, out VisionSource imageSource, out ImageAnalyzer analyzer)
+            {
+
+                // Get image analysis
+                imageSource = VisionSource.FromFile(imageFile);
+                analyzer = new ImageAnalyzer(serviceOptions, imageSource, analysisOptions);
+                var result = analyzer.Analyze();
+
+                if (result.Reason == ImageAnalysisResultReason.Analyzed)
+                {
+                    // Get people in the image
+                    if (result.People != null)
+                    {
+                        Console.WriteLine($" People:");
+
+                        // Prepare image for drawing
+                        System.Drawing.Image image = System.Drawing.Image.FromFile(imageFile);
+                        Graphics graphics = Graphics.FromImage(image);
+                        Pen pen = new Pen(Color.Cyan, 3);
+                        Font font = new Font("Arial", 16);
+                        SolidBrush brush = new SolidBrush(Color.WhiteSmoke);
+
+                        foreach (var person in result.People)
+                        {
+                            // Draw object bounding box if confidence > 50%
+                            if (person.Confidence > 0.5)
+                            {
+                                // Draw object bounding box
+                                var r = person.BoundingBox;
+                                Rectangle rect = new Rectangle(r.X, r.Y, r.Width, r.Height);
+                                graphics.DrawRectangle(pen, rect);
+
+                                // Return the confidence of the person detected
+                                Console.WriteLine($"   Bounding box {person.BoundingBox}, Confidence {person.Confidence:0.0000}");
+                            }
+                        }
+
+                        // Save annotated image
+                        String output_file = "detected_people.jpg";
+                        image.Save(output_file);
+                        Console.WriteLine("  Results saved in " + output_file + "\n");
+                    }
+                }
+                else
+                {
+                    var errorDetails = ImageAnalysisErrorDetails.FromResult(result);
+                    Console.WriteLine(" Analysis failed.");
+                    Console.WriteLine($"   Error reason : {errorDetails.Reason}");
+                    Console.WriteLine($"   Error code : {errorDetails.ErrorCode}");
+                    Console.WriteLine($"   Error message: {errorDetails.Message}\n");
+                }
+            }
 
         }
 
